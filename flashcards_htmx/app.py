7ab0e23246c8cf0106569f9d0d0accfda2c31839
Cyril Dutrieux (cydu) <cydu@odoo.com>
2023-05-07 10:13:58 +0200

    Format with black

--- flashcards_htmx/app.py
+++ flashcards_htmx/app.py
@@ -12,7 +12,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
-__version__ = importlib.metadata.version('flashcards_htmx')
+__version__ = importlib.metadata.version("flashcards_htmx")
 
 
 # Create the FastAPI app
@@ -21,11 +21,14 @@ app = FastAPI(
     description="API Docs for flashcards-htmx",
     version=__version__,
 )
-app.mount("/static", StaticFiles(directory=Path(__file__).parent / "static"), name="static")
+app.mount(
+    "/static", StaticFiles(directory=Path(__file__).parent / "static"), name="static"
+)
 
 
 def get_jinja2():
-    """ Get Jinja2 dependency function. you can define more functions, filters or global vars here """
+    """Get Jinja2 dependency function. you can define more functions, filters or global vars here"""
+
     @pass_context
     def url_for(context: dict, name: str, **path_params: typing.Any) -> str:
         request = context["request"]
@@ -38,24 +41,26 @@ def get_jinja2():
     return env
 
 
-def template(tpl : str):
-    """ Get view render function using Jinja2 environment injected above """
-    def func_view(request: Request, env : Environment = Depends(get_jinja2)):
+def template(tpl: str):
+    """Get view render function using Jinja2 environment injected above"""
+
+    def func_view(request: Request, env: Environment = Depends(get_jinja2)):
         template = env.get_template(tpl)
+
         def render(*args, **kwargs):
             return template.render(request=request, *args, **kwargs)
+
         return render
+
     return func_view
 
 
-
-from flashcards_htmx.public_pages import router as public_router # noqa: F401, E402
-from flashcards_htmx.private_pages import router as private_router # noqa: F401, E402
-from flashcards_htmx.private_components import router as private_components # noqa: F401, E402
+from flashcards_htmx.public_pages import router as public_router  # noqa: F401, E402
+from flashcards_htmx.private_pages import router as private_router  # noqa: F401, E402
+from flashcards_htmx.private_components import (
+    router as private_components,
+)  # noqa: F401, E402
 
 app.include_router(public_router)
 app.include_router(private_router)
 app.include_router(private_components)
-
-
-
--- flashcards_htmx/public_pages.py
+++ flashcards_htmx/public_pages.py
@@ -9,40 +9,44 @@ router = APIRouter()
 
 
 @router.get("/", response_class=HTMLResponse)
-async def landing_page(render = Depends(template('public/landing-page.html'))):
-	return render()
-	
+async def landing_page(render=Depends(template("public/landing-page.html"))):
+    return render()
+
 
 @router.get("/login", response_class=HTMLResponse)
-async def login_page(render = Depends(template('public/login.html'))):
-	return render()
+async def login_page(render=Depends(template("public/login.html"))):
+    return render()
 
 
 @router.get("/signup", response_class=HTMLResponse)
-async def signup_page(render = Depends(template('public/signup.html'))):
-	return render()
+async def signup_page(render=Depends(template("public/signup.html"))):
+    return render()
 
 
 @router.get("/reset-password", response_class=HTMLResponse)
-async def reset_password_page(render = Depends(template('public/reset-password.html'))):
-	return render()
-
-
+async def reset_password_page(render=Depends(template("public/reset-password.html"))):
+    return render()
 
 
 @router.post("/login", response_class=RedirectResponse)
 async def login_action(request: Request):
-	# FIXME Actually do the login!
-	return RedirectResponse(request.url_for('home_page'), status_code=status.HTTP_302_FOUND)
+    # FIXME Actually do the login!
+    return RedirectResponse(
+        request.url_for("home_page"), status_code=status.HTTP_302_FOUND
+    )
 
 
 @router.post("/signup", response_class=RedirectResponse)
 async def signup_action(request: Request):
-	# FIXME Actually do the signup!
-	return RedirectResponse(request.url_for('home_page'), status_code=status.HTTP_302_FOUND)
+    # FIXME Actually do the signup!
+    return RedirectResponse(
+        request.url_for("home_page"), status_code=status.HTTP_302_FOUND
+    )
 
 
 @router.post("/reset-password", response_class=RedirectResponse)
 async def reset_password_action(request: Request):
-	# FIXME Actually do the password reset!
-	return RedirectResponse(request.url_for('home_page'), status_code=status.HTTP_302_FOUND)
+    # FIXME Actually do the password reset!
+    return RedirectResponse(
+        request.url_for("home_page"), status_code=status.HTTP_302_FOUND
+    )
